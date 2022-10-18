ecfec225e65b071bb0ae0cd4ea440e9561c4628f
Jessica Ernst <99251460+Belladonnixi@users.noreply.github.com>
2022-10-18 14:15:56 +0200

    entities done

---
+++ TheVeggie/CoreData/CoreDataManager.swift
@@ -0,0 +1,24 @@
+//
+//  Project: TheVeggie
+//  CoreDataManager.swift
+//
+//
+//  Created by Jessica Ernst on 17.10.22
+//
+/// Copyright © 2022 Jessica Ernst. All rights reserved.
+//
+
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+//    let persistentContainer: NSPersistentContainer
+//    static let shared = CoreDataManager()
+//
+//    var viewContext: NSManagedObjectContext {
+//        return persistentContainer.viewContext
+//    }
+//
+}
